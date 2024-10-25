6e90bbe5f683a48c99cc7254b34aeae120540dad
shaaba762x <shaaba777@gmail.com>
2024-10-25 11:30:30 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -11,18 +11,3 @@ def get_multiplied_digits(number):
 
 result = get_multiplied_digits('4025088')
 print(result)
-
-
-
-# result = get_multiplied_digits(40203) # запускает функцию с параметрами def get_multiplied_digits(*number)
-# def get_multiplied_digits(4, 0, 2, 0, 3)
-# работа функции
-# str_number = str(number) # присваивает переменной str_number строчное значение параметров функции
-# те должно быть так str_number = 40203
-# first = int(str_number[0]) # - переменной first присваевается целочисленное значение строчной переменной
-# str_number = 40203 , те должно быть 4
-# где, Святая Корова, у меня ошибка
-# что я не так понимаю?
-
-
-
